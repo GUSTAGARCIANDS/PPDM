cdf0fed80df240961ea59f85c08c39005a708964
GUSTAGARCIANDS <159182569+GUSTAGARCIANDS@users.noreply.github.com>
2025-03-20 13:41:06 -0300

    Delete calculadoraInteligente directory

--- calculadoraInteligente/ex3.dart
+++
@@ -1,5 +0,0 @@
-void main(){
-  int numero = 10;
-  String resultado = numero % 2 == 0 ? 'par' : 'ímpar';
- print("O resultado é :  $resultado");
-}
--- calculadoraInteligente/ex6.dart
+++
@@ -1,6 +0,0 @@
-void main() {
-  int numero = 5;
-  for (int i = 1; i <= numero; i++) {
-    print("Número: $i");
-  }
-}
